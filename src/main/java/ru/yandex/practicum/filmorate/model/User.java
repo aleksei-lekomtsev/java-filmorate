961727c3d50903a9483d2f5182a7528ed24f37c5
Aleksei Lekomtsev <alexey.lekomcev@yandex.ru>
2023-04-17 16:34:00 +0300

    sprint 11, version 1

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -8,8 +8,6 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PastOrPresent;
 import javax.validation.constraints.Pattern;
 import java.time.LocalDate;
-import java.util.HashSet;
-import java.util.Set;
 
 
 @Data
